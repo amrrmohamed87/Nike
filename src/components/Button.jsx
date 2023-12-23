95da2a5be7768237242f479a0f19538a4dd259bc
Amr <frank1276smith@gmail.com>
2023-12-23 18:28:25 +0200

    adding functionality to rendering icons on button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="shopButton">
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
