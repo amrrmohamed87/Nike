c77ee870d12e6b8d3544c144f693a096f5b0400a
Amr <frank1276smith@gmail.com>
2023-12-23 11:53:46 +0200

    importing components

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,37 @@ const App = () => {
     <main className="realtive">
       Nav
       {/* Hero Section */}
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="xl:padding-1 wide:padding-r padding-b<">
+        <Hero />
+      </section>
       {/* Popular Products Section */}
-      <section className="padding">Popular Products</section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
       {/* Super quality Section */}
-      <section className="padding">SuperQuality</section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
       {/* Services Section */}
-      <section className="padding-x py-10">Services</section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
       {/* Special offers Section */}
-      <section className="padding">SpecialOffers</section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
       {/* Customer Reviews Section */}
-      <section className="bg-pale-blue padding">CustomerReviews</section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
       {/* Subscribe Section */}
-      <section className="padding-x sm:py-32 py:16 w-full">Subscribe</section>
+      <section className="padding-x sm:py-32 py:16 w-full">
+        <Subscribe />
+      </section>
       {/* Footer Section */}
-      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
     </main>
   );
 };
