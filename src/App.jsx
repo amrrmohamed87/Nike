fa4d3a963716062920ce554886902960d7de94a6
Amr <frank1276smith@gmail.com>
2023-12-23 11:26:22 +0200

    created all of the sections of our website

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <main className="realtive">
+      Nav
+      {/* Hero Section */}
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      {/* Popular Products Section */}
+      <section className="padding">Popular Products</section>
+      {/* Super quality Section */}
+      <section className="padding">Super Quality</section>
+      {/* Services Section */}
+      <section className="padding-x py-10">Services</section>
+      {/* Special offers Section */}
+      <section className="padding">Special Offers</section>
+      {/* Customer Reviews Section */}
+      <section className="bg-pale-blue padding">Customer Reviews</section>
+      {/* Subscribe Section */}
+      <section className="padding-x sm:py-32 py:16 w-full">Subscribe</section>
+      {/* Footer Section */}
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
+  );
+};
+
+export default App;
