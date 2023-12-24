41e55681a24ad3e0a3a8fac49fdb53dea027f1d6
Amr <frank1276smith@gmail.com>
2023-12-24 10:52:38 +0200

    deploying subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,21 @@
+import Button from "../components/Button";
 const Subscribe = () => {
-  return <div></div>;
+  return (
+    <section
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+      id="contact-us"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+        Sign Up for <span className="text-coral-red">Update</span> & NewSletter
+      </h3>
+      <div className="subscribeButtonAndInput">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex masx-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
